0f338ec2c928e375cec7894e5203f941e1239ff7
Moon Jam <wu.messi.wu@gmail.com>
2024-02-08 22:32:14 +0800

    add todo

--- cogs/slash.py
+++ cogs/slash.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.app_commands import Choice
 from prettytable import PrettyTable
 
+# TODO: switch language to English
 
 class Slash(commands.Cog):
     def __init__(self, bot: commands.Bot):
